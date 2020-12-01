150c7418993835a3b20f0423038d7255e856cef3
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-01 22:21:23 +0500

    updations

--- src/Regestration/Regestration.jsx
+++ src/Regestration/Regestration.jsx
@@ -12,30 +12,60 @@ status:""
 export const reducer = (state,action)=>
 {
     switch (action.type) {
-        case "":
-            
-            break;
-    
+        
+            case "name":
+            return{
+                ...state,
+                name : action.payload
+            }
+            case "email":
+            return{
+                ...state,
+                email:action.payload
+            }
+            case "gender":
+            return{
+                ...state,
+                gender:action.payload
+            }
+            case "DOB":
+            return{
+                ...state,
+                DOB:action.payload
+            }
+            case "phone":
+            return{
+                ...state,
+                phone:action.payload
+            }
+            case "status":
+            return{
+                ...state,
+                status:action.payload
+            }
         default:
-            break;
+            return{
+                ...state
+            }
+
     }
 
 }
 
  export function Regestration() {
-    const[state, dispatch]= React.useReducer(reducer, formData)
+    const[data, dispatch]= React.useReducer(reducer, formData)
 
     return (
         <>
-           <form className="regestration-form">
-               <input type="text" name="name" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="Enter your name "  />
-               <input type="email" name="email"  placeholder="Enter your email "  />
-               <input type="text" name="gender" placeholder="Enter your gender "  />
-               <input type="text" name="DOB" placeholder="age: dd/mm/yy"  />
-               <input type="phone" name="phone" placeholder="03XXXXXXXXX"/>
-               <input type="text" name="status" placeholder="single/married"/>
+           <form  onSubmit={(e)=>{e.preventDefault()}} method="post" className="regestration-form">
+               <input type="text" required name="name" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="Enter your name "  />
+               <input type="email" required name="email" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="Enter your email "  />
+               <input type="text" required name="gender" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="Enter your gender "  />
+               <input type="text" required  name="DOB" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="age: dd/mm/yy"  />
+               <input type="phone" required name="phone" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="03XXXXXXXXX"/>
+               <input type="text" required name="status" onChange={(e)=>{dispatch({type:e.target.name, payload:e.target.value })}} placeholder="single/married"/>
                <button>submit</button> 
-               {console.log(state)}   
+               {console.log(data)}   
             </form> 
         </>
     )
