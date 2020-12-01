92d03d2dba2356205201debbc45348b935dfb9ca
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-01 18:44:09 +0500

    adding items

---
+++ src/Regestration/Regestration.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import  "../App.css";
+ const formData={
+name:"",
+email:"",
+gender:"",
+DOB:"",
+phone:"",
+status:""
+
+}
+export const reducer = (state,action)=>
+{
+    switch (action.type) {
+        case "":
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+ export function Regestration() {
+    const[state, dispatch]= React.useReducer(reducer, formData)
+
+    return (
+        <>
+           <form className="regestration-form">
+               <input type="text" name="name" onChange={()=>{dispatch({type:"name", payload:{ } })}} placeholder="Enter your name "  />
+               <input type="email" name="email"  placeholder="Enter your email "  />
+               <input type="text" name="gender" placeholder="Enter your gender "  />
+               <input type="text" name="DOB" placeholder="age: dd/mm/yy"  />
+               <input type="phone" name="phone" placeholder="03XXXXXXXXX"/>
+               <input type="text" name="status" placeholder="single/married"/>
+               <button>submit</button> 
+               {console.log(state)}   
+            </form> 
+        </>
+    )
+}
